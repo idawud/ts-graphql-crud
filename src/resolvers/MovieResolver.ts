1b80a840970bde23b3b889872f2620e2449acfc7
Dawud Ibrahim Ismail <ismaildawud96@gmail.com>
2020-07-18 11:52:34 +0000

    mutate with inputTypes

--- src/resolvers/MovieResolver.ts
+++ src/resolvers/MovieResolver.ts
@@ -1,13 +1,21 @@
-import { Resolver, Mutation, Arg, Float, Query } from "type-graphql"; 
+import { Resolver, Mutation, Arg, Float, Query, Field, InputType } from "type-graphql"; 
 import { Movie } from "../entity/Movie";
+
+@InputType()
+class MovieInput{
+    @Field()
+    title: string;
+
+    @Field(() => Float)
+    minutes: number;
+}
 
 @Resolver()
 export class MovieResolver {
-    @Mutation(() => Boolean)
-    async createMovie(@Arg('title') title: string, @Arg('minutes', () => Float) minutes: number){
-        await Movie.insert({title, minutes});
-        console.log(`${title} ${minutes} created`);
-        return true;
+    @Mutation(() => Movie)
+    async createMovie(@Arg('options', () => MovieInput) options: MovieInput ) {
+        const movie: Movie = await Movie.create(options).save();
+        return movie;
     }
 
     @Query(() => [Movie])
